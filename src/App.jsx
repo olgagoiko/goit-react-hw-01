4b9a0b828d39a9b369f1e6645bae8fff4724c20b
Olga Goiko <olga.a10@ukr.net>
2024-09-02 20:13:58 +0300

    add files to task 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Profile from './components/Profile/Profile';
 import userData from './userData.json';
 import friends from './friends.json';
 import FriendList from './components/FriendList/FriendList';
+import transactions from './transactions.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 const App = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
